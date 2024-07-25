4996cd97e66fa14999c493e5901c1acbe30c712d
lenevio1337 <difeliceardente.nevio@gmail.com>
2024-07-26 01:19:13 +0200

    commit

--- app/src/main/java/de/lenevio1337/banking/MainActivity.kt
+++ app/src/main/java/de/lenevio1337/banking/MainActivity.kt
@@ -1,10 +1,12 @@
 package de.lenevio1337.banking
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val registerTextView = findViewById<TextView>(R.id.textView)
+        registerTextView.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/de/lenevio1337/banking/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package de.lenevio1337.banking
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
